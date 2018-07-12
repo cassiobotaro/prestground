5fa83c10e566a0c2882013e472fd54d314b6e12b
cassiobotaro <cassiobotaro@gmail.com>
2018-07-12 17:25:25 -0300

    apply middleware only in certain custom routes

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/prest/cmd"
 	"github.com/prest/config"
 	"github.com/prest/config/router"
+	"github.com/urfave/negroni"
 )
 
 func main() {
@@ -19,6 +21,14 @@ func main() {
 	// handler overload
 	r.HandleFunc("/{database:demo}/{schema:public}/{table:person}", OverloadedHandler).Methods("GET")
 
+	// custom middleware applied in just one endpoint
+	adminRoutes := mux.NewRouter().PathPrefix("/admin").Subrouter()
+	adminRoutes.HandleFunc("/secret", SecretHandler)
+	r.PathPrefix("/").Handler(negroni.New(
+		negroni.HandlerFunc(adminOnly),
+		negroni.Wrap(adminRoutes),
+	))
+
 	// Call pREST cmd
 	cmd.Execute()
 }
@@ -31,4 +41,17 @@ func Pong(w http.ResponseWriter, r *http.Request) {
 // OverloadedHandler just change prest default endpoint
 func OverloadedHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("nothing to see here!"))
+}
+
+// SecretHandler hides a secret
+func SecretHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("import this!"))
+}
+
+func adminOnly(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if r.Header.Get("X-user") != "admin" {
+		http.Error(w, "nothing to see here", http.StatusUnauthorized)
+		return
+	}
+	next(w, r)
 }
